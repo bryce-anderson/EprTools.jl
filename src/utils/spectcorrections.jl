1af77b7fc422ba54072e84413746108538ccd942
Bryce Anderson <bryce.anderson22@gmail.com>
2013-11-25 14:53:49 -0500

    Small improvements

--- src/utils/spectcorrections.jl
+++ src/utils/spectcorrections.jl
@@ -21,17 +21,40 @@ function fixProjectionOffset(data::Vector, tolerance::Real = 0.05)
     return data
 end
 
-function fix2DTrace(data::Array, t0index::Int, starting::Int, ending::Int, phase::Real = 0.0)
-    I = (exp(-phase/180.0im*pi)*fixOffset(data, t0index))
+function fix2DTrace{T}(data::Array{T,2}, t0index::Int, starting::Int, ending::Int; phase0::Real = 0.0, phase1::Real = 0.0)
+    I = copy(data)
+    
+    fixVector = linearPhaseCorrection(T, size(I)[2], phase0, phase1)'
+    
+    for i = 1:size(I)[1]
+      I[i,:] = I[i,:].*fixVector
+    end
+    
+    I = fixOffset(I, t0index)
+    #, phase0, phase1)
     avgIr = flatten(mean(real(I[starting:ending,:]), 1)[1,:])
-    println("Shape: $(size(avgIr))")
+    
     avgIr = fixProjectionOffset(avgIr)
-    avgIr = avgIr/maximum(abs(avgIr))
+    scale = maximum(abs(avgIr))   # We want them to be on the same scale, so we arbitrarily scale to the real.
+    avgIr = avgIr/scale
     
     avgIi = flatten(mean(imag(I[1000:4000,:]), 1)[1,:])
     avgIi = fixProjectionOffset(avgIi)
-    avgIi = avgIi/maximum(abs(avgIi))
+    avgIi = avgIi/scale
     
     return avgIr + avgIi*im
+end
+
+# Phase shifts the spectrum by multiplying by exp(-p0*pi*im/180) *
+#   exp(-p1*pi*im/180)
+function linearPhaseCorrection(T::Type, length, p0::Real, p1::Real = 0.0)
+    assert(length > 0)
+    
+    fix = ones(T, length)*exp(-pi*p0*im/180.0)
+    
+    range = linspace(-1.0, 1.0, length)
+    linear = exp(-range*pi*p1*im/180.0)
+    fix = fix.*linear
+    return convert(Vector{T}, fix)
 end
     
